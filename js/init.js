dd60dff80e594900e79dd49a5b19218d55f1d8aa
formerick <152239657+formerick@users.noreply.github.com>
2023-12-18 20:12:37 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,25 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCnRLAKAAjmoJeG1UzEZcFzP0fi7DjU0wQ",
+
+    authDomain: "bdalumnoseaml.firebaseapp.com",
+
+    projectId: "bdalumnoseaml",
+
+    storageBucket: "bdalumnoseaml.appspot.com",
+
+    messagingSenderId: "1095855594364",
+
+    appId: "1:1095855594364:web:a3443cd956e7571621e2f9",
+
+    measurementId: "G-7T05X8J13D"
+
+});
